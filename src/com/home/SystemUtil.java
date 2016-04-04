8bf08238f7da9c1ffe0bed3a976f57173e89597c
Manasvi Kamishetty <manasvij2ee@gmail.com>
2016-04-04 10:52:40 -0400

    Added two files.

---
+++ src/com/home/SystemUtil.java
@@ -0,0 +1,41 @@
+package com.home;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SystemUtil {
+	public static boolean emailSimpleValidation() {
+
+		return false;
+
+	}
+
+	public static boolean emailRegExpValidation(String emailId) {
+		String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(emailPattern);
+		Matcher matcher = pattern.matcher(emailId);
+		return matcher.matches();
+	}
+	
+	public static boolean passwordValidation(String pass1,String pass2){
+		if(pass1.equals(pass2)){
+			return true;
+		} else{
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		String emailId = "svsdvsdbsdbmail.com";
+		String password = "vivek12cdv3";
+		String confirmPassword = "vivek123";	
+		System.out.println(passwordValidation(password, confirmPassword));
+		System.out.println(emailRegExpValidation(emailId));
+		
+		boolean isEverythingOk = false;
+		isEverythingOk = SystemUtil.emailRegExpValidation(emailId) && SystemUtil.passwordValidation(password, confirmPassword);
+		System.out.println(isEverythingOk);
+	}
+	
+}
